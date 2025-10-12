c5dc1451ed71ca883b0c898f15fcfbc5867380ad
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-12 15:48:55 -0500

    feat: Add devices relations with client
    Add device property in client class

--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/users/Client.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/users/Client.java
@@ -1,11 +1,20 @@
 package com.backend.agrosensor.agrosensorbackend.entity.impl.users;
 
 import com.backend.agrosensor.agrosensorbackend.entity.base.AbstractUser;
+import com.backend.agrosensor.agrosensorbackend.entity.base.device.Device;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Table(name = "clients")
+@Data
 public class Client extends AbstractUser {
-    private String propiedadCliente;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "client")
+    private List<Device> devices;
 }
