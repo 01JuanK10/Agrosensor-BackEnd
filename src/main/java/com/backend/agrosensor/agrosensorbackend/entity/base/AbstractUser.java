01a8a0d2ec1377be06bc0f3213e528124227823d
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-12 15:54:20 -0500

    config: Add new Users config
    Add JsonProperty for security and performance in sensitive information

--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/base/AbstractUser.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/base/AbstractUser.java
@@ -1,18 +1,26 @@
 package com.backend.agrosensor.agrosensorbackend.entity.base;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "users")
 @Data
 public abstract class AbstractUser {
+
     @Id
     private Integer cc;
     private String name;
     private String lastname;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String role;
 }
