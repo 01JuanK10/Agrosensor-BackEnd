f57be08d32e1bddd5e34ee8e632f91efa032bb29
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-11 22:32:53 -0500

    First commit

---
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/AgrosensorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.agrosensor.agrosensorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrosensorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrosensorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/agrosensor/agrosensorbackend/AgrosensorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.agrosensor.agrosensorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosensorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
