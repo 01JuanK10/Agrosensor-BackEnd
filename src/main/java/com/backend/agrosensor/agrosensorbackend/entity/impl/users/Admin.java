1987079376249f5b137d533ac05b705f0ec0c769
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-12 15:50:22 -0500

    feat: Add property to admin
    Add 'enterprise' property for admin information

--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/users/Admin.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/users/Admin.java
@@ -3,10 +3,13 @@ package com.backend.agrosensor.agrosensorbackend.entity.impl.users;
 import com.backend.agrosensor.agrosensorbackend.entity.base.AbstractUser;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "admins")
 
+@Data
 public class Admin extends AbstractUser {
-    private String propiedadAdmin;
+
+    private String enterprise;
 }
