735487b0933372b9159891caf82382c9f80f5bbb
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-14 14:29:40 -0500

    feat: add measurement repository
    Add persistence repository for SoilMeasurement class

--- src/main/java/com/backend/agrosensor/agrosensorbackend/repository/measurements/ISoilMeasurementsRepository.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/repository/measurements/ISoilMeasurementsRepository.java
@@ -1,4 +1,9 @@
 package com.backend.agrosensor.agrosensorbackend.repository.measurements;
 
-public interface ISoilMeasurementsRepository {
+import com.backend.agrosensor.agrosensorbackend.entity.impl.measurements.SoilMeasurements;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISoilMeasurementsRepository extends JpaRepository<SoilMeasurements,Integer> {
 }
