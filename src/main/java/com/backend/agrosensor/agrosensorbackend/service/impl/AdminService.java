3badbf619af6cf29123b52dc0db713cc0e9fb7c1
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-14 17:07:38 -0500

    Feat: Add the implementation for the interface methods
    Add methods create, findbyId, findAll and update.

--- src/main/java/com/backend/agrosensor/agrosensorbackend/service/impl/AdminService.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/service/impl/AdminService.java
@@ -1,32 +1,43 @@
 package com.backend.agrosensor.agrosensorbackend.service.impl;
 
 import com.backend.agrosensor.agrosensorbackend.entity.impl.users.Admin;
+import com.backend.agrosensor.agrosensorbackend.repository.users.IAdminRepository;
 import com.backend.agrosensor.agrosensorbackend.service.base.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AdminService implements IUserService<Admin> {
+    @Autowired
+    private IAdminRepository adminRepository;
 
     @Override
     public Admin create(Admin user) throws RuntimeException {
-        return null;
+        if (adminRepository.findById(user.getCc()).isPresent()) {
+            throw new RuntimeException();
+        }
+        return adminRepository.save(user);
     }
 
     @Override
     public Admin findByCc(Long cc) throws RuntimeException {
-        return null;
+        return adminRepository.findById(cc)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 
     @Override
     public List<Admin> findAll() {
-        return List.of();
+        return adminRepository.findAll();
     }
 
     @Override
     public Admin update(Admin user) throws RuntimeException {
-        return null;
+        if (adminRepository.findById(user.getCc()).isEmpty()) {
+            throw new RuntimeException();
+        }
+        return adminRepository.save(user);
     }
 
     @Override
