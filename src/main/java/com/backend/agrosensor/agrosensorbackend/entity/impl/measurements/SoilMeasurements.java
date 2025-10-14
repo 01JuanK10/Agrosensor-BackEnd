0f174c5de8652be403417bd71948c405ef42011b
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-14 14:05:23 -0500

    refact: remove unused imports

--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/measurements/SoilMeasurements.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/measurements/SoilMeasurements.java
@@ -2,8 +2,6 @@ package com.backend.agrosensor.agrosensorbackend.entity.impl.measurements;
 
 import com.backend.agrosensor.agrosensorbackend.entity.base.Measurement;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.Data;
 
--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/notifications/AppNotification.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/notifications/AppNotification.java
@@ -2,8 +2,6 @@ package com.backend.agrosensor.agrosensorbackend.entity.impl.notifications;
 
 import com.backend.agrosensor.agrosensorbackend.entity.base.Notification;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 
 
