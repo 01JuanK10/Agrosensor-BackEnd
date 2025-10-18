9014e4688ae01ea38e783485f011295df3c000a1
JuanK <camiloalzatebedoya15@gmail.com>
2025-10-18 10:25:05 -0500

    Perf: change notification relations
    Add new relation between appNotification and client

--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/users/Client.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/impl/users/Client.java
@@ -1,11 +1,9 @@
 package com.backend.agrosensor.agrosensorbackend.entity.impl.users;
 
 import com.backend.agrosensor.agrosensorbackend.entity.base.AbstractUser;
+import com.backend.agrosensor.agrosensorbackend.entity.base.Notification;
 import com.backend.agrosensor.agrosensorbackend.entity.base.device.Device;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
@@ -17,4 +15,9 @@ public class Client extends AbstractUser {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "client")
     private List<Device> devices;
+
+    @OneToMany
+    @JoinColumn(name = "notification_id")
+    private List<Notification> notifications;
+
 }
