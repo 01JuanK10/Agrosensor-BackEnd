6e4043bbaae0d8899c6c329a9572673f66116dba
JuanK <camiloalzatebedoya15@gmail.com>
2025-11-25 11:18:29 -0500

    update device info

--- src/main/java/com/backend/agrosensor/agrosensorbackend/entity/base/device/Device.java
+++ src/main/java/com/backend/agrosensor/agrosensorbackend/entity/base/device/Device.java
@@ -1,15 +1,26 @@
 package com.backend.agrosensor.agrosensorbackend.entity.base.device;
 
+import java.util.List;
+
 import com.backend.agrosensor.agrosensorbackend.entity.base.Measurement;
 import com.backend.agrosensor.agrosensorbackend.entity.impl.devices.Esp32;
 import com.backend.agrosensor.agrosensorbackend.entity.impl.users.Client;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import jakarta.persistence.*;
-import lombok.Data;
 
-import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 
 @JsonTypeInfo(
@@ -29,6 +40,7 @@ public abstract class Device {
     @Id
     private String id;
     private String type;
+    private Boolean active;
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     @JoinColumn(name = "location_id")
